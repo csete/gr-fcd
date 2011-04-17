3689c98f05c2e6b563dc94fb77df70c64189b287
Alexandru Csete <oz9aec@gmail.com>
2011-04-17 16:13:18 +0200

    Use boost::program_options to specify audio device, frequency and LNA gain in example receiver.

--- apps/fcd_nfm_rx.cc
+++ apps/fcd_nfm_rx.cc
@@ -27,6 +27,9 @@
  * audio device for output.
  *
  */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 // Include header files for each block used in flowgraph
 #include <gr_top_block.h>
@@ -36,6 +39,12 @@
 #include <gr_audio_sink.h>
 #include <fcd_source_c.h>
 
+// other includes
+#include <iostream>
+#include <boost/program_options.hpp>
+
+
+namespace po = boost::program_options;
 
 int main(int argc, char **argv)
 {
@@ -43,11 +52,37 @@ int main(int argc, char **argv)
     float pi = 3.141592654;
 
 
+    //variables to be set by po
+    std::string device;
+    int freq;
+    float gain;
+
+    //setup the program options
+    po::options_description desc("Command line options");
+    desc.add_options()
+        ("help", "This help message")
+        ("device", po::value<std::string>(&device)->default_value("hw:1"), "Audio input device")
+        ("freq", po::value<int>(&freq)->default_value(145500), "RF frequency in kHz")
+        ("gain", po::value<float>(&gain)->default_value(20.0), "LNA gain in dB")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    //print the help message
+    if (vm.count("help")){
+        std::cout << "Narrow band FM receiver example" << std::endl << desc << std::endl;
+        return ~0;
+    }
+
+
     // Construct a top block that will contain flowgraph blocks.
     gr_top_block_sptr tb = gr_make_top_block("fcd_nfm_rx");
 
     // FCD source
-    fcd_source_c_sptr fcd = fcd_make_source_c("hw:1");
+    fcd_source_c_sptr fcd = fcd_make_source_c(device);
+    fcd->set_freq_khz(freq);
+    fcd->set_lna_gain(gain);
     
     // Low pass filter
     std::vector<float> taps = gr_firdes::low_pass(1.0, 96000, 5000.0, 1000.0);
